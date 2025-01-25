e95dc7f6cede35c36bbbe87da7aa70449758c194
Jeevitha-Thangaraj <jiveethat@gmail.com>
2025-01-25 14:13:00 +0530

    upgrade

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer): 
     class Meta:
         model = User
-        fields = ["username","password","id"]
+        fields ="__all__"
     
     def create(self, validated_data):
         user = User.objects.create_user(
